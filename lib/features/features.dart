2561737a9f4468dc9afbc4bda86d3dc447743129
Mahmoud Abdullah Younis <mahmoud.abedallah654@science.aun.edu.eg>
2026-02-11 00:31:51 +0200

    Feature/project intialization (#4)
    * feat(development): Finish project Intialization
    
    * feat(development): Finish project Intialization

---
+++ lib/features/features.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class Features extends StatelessWidget {
+  const Features({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
