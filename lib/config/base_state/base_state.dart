92676fac32bdbca4e190bdafd0ab8787c1953b71
Mahmoud Younis <mahmoud.abedallah654@science.aun.edu.eg>
2026-02-11 16:35:49 +0200

    refactor(intialize): Add is Empty variable to the base satat

--- lib/config/base_state/base_state.dart
+++ lib/config/base_state/base_state.dart
@@ -3,16 +3,18 @@ import 'package:equatable/equatable.dart';
 class BaseState<T> extends Equatable {
   final T? data;
   final String? errorMessage;
+  final bool? isEmpty;
 
-  const BaseState({this.data, this.errorMessage});
+  const BaseState({this.data, this.errorMessage, this.isEmpty});
 
-  BaseState<T> copyWith({T? data, String? errorMessage}) {
+  BaseState<T> copyWith({T? data, String? errorMessage, bool? isEmpty}) {
     return BaseState<T>(
       data: data ?? this.data,
       errorMessage: errorMessage ?? this.errorMessage,
+      isEmpty: isEmpty ?? this.isEmpty,
     );
   }
 
   @override
-  List<Object?> get props => [data, errorMessage];
+  List<Object?> get props => [data, errorMessage, isEmpty];
 }
