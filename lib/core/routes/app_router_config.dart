a9838ad7652dffe0e291582e1c56524dc17dad50
omar khaled <omarkhaled10102004@gmail.com>
2026-02-16 13:31:07 +0200

    feat(forget password): Implement constants and routing

--- lib/core/routes/app_router_config.dart
+++ lib/core/routes/app_router_config.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
+import '../../features/auth/forget_password/presentation/onbourding_screen.dart';
+import '../../features/auth/forget_password/presentation/view/screens/forget_password_screen.dart';
 import '../constants/app_text_string.dart';
 import 'app_routes.dart';
 
@@ -17,6 +18,16 @@ class AppRouterConfig {
       //   name: AppRoutes.onboardingRoute,
       //   builder: (context, state) => const TestWidget(),
       // ),
+      GoRoute(
+        path: AppRoutes.forgetPasswordRoute,
+        name: AppRoutes.forgetPasswordRoute,
+        builder: (context, state) => const ForgetPasswordScreen(),
+      ),
+      GoRoute(
+        path: AppRoutes.onboardingRoute,
+        name: AppRoutes.onboardingRoute,
+        builder: (context, state) => const OnbourdingScreen(),
+      ),
     ],
   );
 }
