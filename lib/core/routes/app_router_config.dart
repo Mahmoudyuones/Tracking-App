e8bad15037a95e770120e0be59ae7a00405b178e
omar khaled <omarkhaled10102004@gmail.com>
2026-02-16 23:10:03 +0200

    feat(forget-password): Implement ui and views

--- lib/core/routes/app_router_config.dart
+++ lib/core/routes/app_router_config.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
+import '../../features/auth/forget_password/presentation/view/screens/forget_password_screen.dart';
 import '../constants/app_text_string.dart';
 import 'app_routes.dart';
 
@@ -15,8 +15,13 @@ class AppRouterConfig {
       // GoRoute(
       //   path: AppRoutes.onboardingRoute,
       //   name: AppRoutes.onboardingRoute,
-      //   builder: (context, state) => const TestWidget()
+      //   builder: (context, state) => const TestWidget(),
       // ),
+      GoRoute(
+        path: AppRoutes.forgetPasswordRoute,
+        name: AppRoutes.forgetPasswordRoute,
+        builder: (context, state) => const ForgetPasswordScreen(),
+      ),
     ],
   );
 }
