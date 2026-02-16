0866ab3f761647595548e2acc895afcef2f10986
omar khaled <omarkhaled10102004@gmail.com>
2026-02-17 00:15:52 +0200

    feat(forget-password): Implement UI and logic (cubit, intents, states)

---
+++ lib/features/auth/forget_password/presentation/view/views/verify_code_view.dart
@@ -0,0 +1,192 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pinput/pinput.dart';
+import '../../../../../../core/constants/app_text_string.dart';
+import '../../../../../../core/style/color/app_colors.dart';
+import '../../../../../../core/utility/ui/ui_utils.dart';
+import '../../view_model/forget_password_cubit/forget_password_cubit.dart';
+import '../../view_model/forget_password_cubit/forget_password_intents.dart';
+import '../../view_model/forget_password_cubit/forget_password_states.dart';
+import '../../view_model/forget_password_cubit/forget_password_ui_intents.dart';
+
+class VerifyCodeView extends StatefulWidget {
+  final VoidCallback onSuccess;
+
+  const VerifyCodeView({super.key, required this.onSuccess});
+
+  @override
+  State<VerifyCodeView> createState() => VerifyCodeViewState();
+}
+
+class VerifyCodeViewState extends State<VerifyCodeView> {
+  StreamSubscription? _subscription;
+  final TextEditingController _otpController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _subscription = context.read<ForgetPasswordCubit>().uiIntents.listen((
+        event,
+      ) {
+        if (!mounted) return;
+        switch (event) {
+          case ShowLoadingVerifyIntent():
+            UIUtils.showEasyLoading();
+          case ShowErrorMsgVerifyIntent():
+            UIUtils.hideLoading(context);
+          case ShowSuccessMsgVerifyIntent(:final message):
+            UIUtils.hideLoading(context);
+            UIUtils.showMessage(
+              message,
+              backGroundColor: AppColors.green,
+              textColor: AppColors.white,
+            );
+          case NavigateToResetPasswordIntent():
+            UIUtils.hideLoading(context);
+            widget.onSuccess();
+          default:
+            break;
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    _otpController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    final defaultPinTheme = PinTheme(
+      width: size.height * .06,
+      height: size.height * .06,
+      textStyle: Theme.of(context).textTheme.titleLarge?.copyWith(
+        color: AppColors.gray,
+        fontWeight: FontWeight.bold,
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.gray.withValues(alpha: .5)),
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+
+    final errorPinTheme = defaultPinTheme.copyDecorationWith(
+      border: Border.all(color: AppColors.red),
+      borderRadius: BorderRadius.circular(12),
+    );
+
+    final focusedPinTheme = defaultPinTheme.copyDecorationWith(
+      border: Border.all(color: AppColors.primary),
+      borderRadius: BorderRadius.circular(12),
+    );
+
+    final submittedPinTheme = focusedPinTheme.copyDecorationWith(
+      border: Border.all(color: AppColors.primary),
+    );
+
+    return BlocBuilder<ForgetPasswordCubit, ForgetPasswordStates>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              Text(
+                AppTextString.emailVerificationHeader,
+                style: Theme.of(
+                  context,
+                ).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                AppTextString.emailVerificationTitle,
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                  color: AppColors.lightTextgrey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: size.height * 0.039),
+              Pinput(
+                controller: _otpController,
+                length: 6,
+                defaultPinTheme: defaultPinTheme,
+                focusedPinTheme: focusedPinTheme,
+                submittedPinTheme: submittedPinTheme,
+                errorPinTheme: errorPinTheme,
+                forceErrorState: state.errorMessage != null,
+                errorText: state.errorMessage,
+                errorBuilder: (errorText, pin) {
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Icon(
+                          Icons.error_outline,
+                          color: AppColors.red,
+                          size: 20,
+                        ),
+                        const SizedBox(width: 4),
+                        Text(
+                          errorText ?? '',
+                          style: Theme.of(context).textTheme.titleMedium
+                              ?.copyWith(color: AppColors.red),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+                showCursor: false,
+                onChanged: (value) {
+                  context.read<ForgetPasswordCubit>().doIntent(
+                    OtpChangedIntent(value),
+                  );
+                },
+                onCompleted: (pin) {
+                  context.read<ForgetPasswordCubit>().doIntent(
+                    SubmitCodeIntent(),
+                  );
+                },
+              ),
+              SizedBox(height: size.height * 0.03),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '${AppTextString.didResentCode} ',
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: AppColors.lightTextgrey,
+                      fontSize: 17,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      context.read<ForgetPasswordCubit>().doIntent(
+                        ResendCodeIntent(state.email),
+                      );
+                    },
+                    child: Text(
+                      AppTextString.resend,
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: AppColors.primary,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
