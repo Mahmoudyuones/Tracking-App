d786751fcfc090e7f6507a1b89ed785856434126
Mahmoud Younis <mahmoud.abedallah654@science.aun.edu.eg>
2026-02-17 03:17:44 +0200

    chore(app): Intergerate with Firebase and edit the baseurl

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,7 +1,12 @@
 class ApiEndpoints {
   ApiEndpoints._();
 
-  static const String baseUrl = 'https://flower.elevateegy.com/api/v1/drivers/';
+  static const String baseUrl = 'https://flower.elevateegy.com/api/v1';
+  static const String idPathQuery = 'Id';
+
   // ------------------------ AUTHENTICATION ENDPOINTS ------------------------ //
-  static const String changePassword = 'change-password';
+  static const String changePassword = '/drivers/change-password';
+
+  // ------------------------ TRACK ORDER ENDPOINTS ------------------------ //
+  static const String updateOrderState = '/orders/state/{Id}';
 }
