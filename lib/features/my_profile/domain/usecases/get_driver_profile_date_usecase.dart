f953bafa6de4be086b4fb4fc6e3caac0f957bc5b
Osama Saad <117564564+osama25s@users.noreply.github.com>
2026-02-18 18:20:46 +0200

    fix(my-profile): Resolve pr comments

--- lib/features/my_profile/domain/usecases/get_driver_profile_date_usecase.dart
+++ lib/features/my_profile/domain/usecases/get_driver_profile_date_usecase.dart
@@ -1,7 +1,9 @@
+import 'package:injectable/injectable.dart';
 import '../../../../config/base_response/base_response.dart';
 import '../entities/driver_response_entity.dart';
 import '../repos/my_profile_repo.dart';
 
+@injectable
 class GetDriverProfileDateUsecase {
   final MyProfileRepo _myProfileRepo;
 
