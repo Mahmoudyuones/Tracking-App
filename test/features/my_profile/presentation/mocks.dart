57a8d1ef4b6a56a96a0656d2828a402daa7b78cb
Osama Saad <117564564+osama25s@users.noreply.github.com>
2026-02-25 22:13:40 +0200

    feat(my-profile): Add presentation layer widget tests for my_profile feature widgets and screens.

---
+++ test/features/my_profile/presentation/mocks.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:tracking_app/features/my_profile/presentation/view_model/my_profile_cubit.dart';
+
+@GenerateMocks([MyProfileCubit])
+void main() {}
