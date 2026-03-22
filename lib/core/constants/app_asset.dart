9646f1d05e96cb8d6f7ecab0687b162799809795
omar khaled <omarkhaled10102004@gmail.com>
2026-03-23 01:07:53 +0200

    feat(profile): Make the ubdate button disabled if the user not change anything

--- lib/core/constants/app_asset.dart
+++ lib/core/constants/app_asset.dart
@@ -2,4 +2,7 @@ class AppAsset {
   AppAsset._();
   static const String translationsPath = 'assets/translations';
   static const String successImage = 'assets/images/success.png';
+  static const String applySuccess = 'assets/images/apply_success.png';
+  static const String applySuccessFooter =
+      'assets/images/apply_success_footer.png';
 }
